24a33fe7a78dd0301c7297378f8654ad310c6118
Matthew Cook <cookm353@gmail.com>
2023-11-17 12:21:07 -0500

    Donezo

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,27 @@
-function findFloor() {
-  
+function findFloor(nums, target) {
+    let low = 0
+    let high = nums.length - 1
+
+    if (target < nums[low]) return -1
+    if (target > nums[high]) return nums[high]
+
+    while (low <= high) {
+        let mid = Math.floor(low + (high + low) / 2)
+
+        if (nums[mid] > target) {
+            if (nums[mid - 1] < target) {
+                return mid - 1
+            } else {
+                high = mid - 1
+            }
+        } else if (nums[mid] < target) {
+            if (nums[mid + 1] > target) {
+                return mid + 1
+            } else {
+                low = mid + 1
+            }
+        }
+    }
 }
 
 module.exports = findFloor
