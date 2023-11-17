b46c063b38a7b8ca194902f736e7c2852dae633c
Matthew Cook <cookm353@gmail.com>
2023-11-17 12:07:21 -0500

    Clean up

--- count-zeroes.js
+++ count-zeroes.js
@@ -3,14 +3,12 @@ function countZeroes(nums) {
     let rightIndex = nums.length - 1
     let numZeroes = 0
 
-    // First check if the array is all 1s or all 0s
     if (nums[0] === 0) {
         return nums.length
     } else if (nums[rightIndex] === 1 ) {
         return 0
     }
 
-    // Find the first 0, then find the number of 
     while (leftIndex <= rightIndex) {
         let midIndex = Math.floor((leftIndex + rightIndex) / 2)
         let midVal = nums[midIndex]
