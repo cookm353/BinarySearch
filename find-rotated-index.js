2b92bf1acaa506fb1990e424cca9836bdcd598e8
Matthew Cook <cookm353@gmail.com>
2023-11-17 11:29:23 -0500

    Donezo

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,46 @@
-function findRotatedIndex() {
- 
+function findPivot(nums) {
+    let low = 0
+    let high = nums.length - 1
+
+    while (low < high) {
+        const mid = Math.floor(low + (high - low) / 2)
+        const midVal = nums[mid]
+
+        if (midVal < nums[low]) {
+            high = mid
+        } else if (midVal > nums[low] && midVal < nums[mid + 1]) {
+            low = mid
+        } else if (midVal > nums[mid - 1] && midVal > nums[mid + 1]) {
+            return mid
+        }
+    }
+}
+
+function findRotatedIndex(nums, target) {
+    let low, high
+    const pivot = findPivot(nums)
+    
+    if (target > nums[0]) {
+        low = 0
+        high = pivot
+    } else {
+        low = pivot
+        high = nums.length - 1
+    }
+
+    while (low <= high) {
+        const mid = Math.floor(low + (high - low) / 2)
+
+        if (nums[mid] > target) {
+            high = mid - 1
+        } else if (nums[mid] < target) {
+            low = mid + 1
+        } else if (nums[mid] === target) {
+            return mid
+        }
+    }
+
+    return -1
 }
 
 module.exports = findRotatedIndex
